80ed5d0af22f59275b3c54f8a1a5f2d860c6b7e9
Ramon Buckland <ramon@thebuckland.com>
2024-03-11 07:54:52 +1100

    attempt to fn S - str

--- tools/pants-plugins/pinchworms/src/pinchworms/general/read_file.py
+++ tools/pants-plugins/pinchworms/src/pinchworms/general/read_file.py
@@ -4,76 +4,28 @@ from pants.engine.target import (
     COMMON_TARGET_FIELDS,
     Target
 )
-from pants.engine.unions import UnionRule
+from pants.engine.unions import UnionRule, union
 from pants.core.target_types import FileSourceField, StringField, MultipleSourcesField
 from pants.util.strutil import help_text
 from dataclasses import dataclass
 from typing import Optional
 import os
 
-# class RegexFilterField(StringField):
-#     alias = "regex"
-#     required = False
-#     help = help_text(
-#         """
-#         Regex pattern, used to extract the string from the file contents.
-#         """
-#     )
-
-class ContentOfMultipleSourcesField(MultipleSourcesField):
-    required = False
-    help = help_text(
-        """
-        A list of files to read the contents from.
-        All contents are concatenated together.
-        """
-    )
-
-
 @dataclass(frozen=True)
-class ReadFileContents(str):
-    """read_file_contents() string provider"""
-
+class ReadFileContents:
     alias = "read_file_contents"
     help = help_text(
         """
-        A string read from a file.
+        Read a string from a file.
         """
     )
+    sources: Optional[MultipleSourcesField] = None
     regex: Optional[str] = None
-    static: Optional[str] = None
-    sources: Optional[ContentOfMultipleSourcesField] = None
-
-    def __new__(cls, value: str = "dummy-value-from-new", **kwargs):
-        print(f"new() {value=} {kwargs=}", end="")
-        return super().__new__(cls, value)
-
-    def __init__(self, value: str = "dummy-value-from-init", **kwargs):
-        print(f"init {value=} {kwargs=}", end="")
-        super().__init__()
-        # self.regex = kwarg.get("regex")
-        # self.static = kwarg.get("static")
-        # self.sources = kwarg.get("sources")
-
-    def __hash__(self):
-        return super().__hash__()
-
-
-@dataclass(frozen=True)
-class ReadFileContentsRequest:
-    target: Target
-
-    @classmethod
-    def is_applicable(cls, _: Target) -> bool:
-       return True
-
 
 @rule
-async def do_read_the_file(request: ReadFileContentsRequest) -> str:
+async def do_read_the_file(file_content_alias: ReadFileContents) -> str:
 
-    return ReadFileContents("winner")
+    return "read-the-file-contents"
 
 def rules():
-    return (
-        do_read_the_file,
-    )
+    return (do_read_the_file,)
